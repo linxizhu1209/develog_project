549abea02933945be1203d5de8fa49a96e7afb47
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-16 11:44:37 +0900

    feat: 초기 설정 구현
    - 프로젝트를 위한 초기 설정 세팅

---
+++ src/test/java/org/log/travel/log_travel_project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.log.travel.log_travel_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
